4cea0500d1468c5479d5d82462cdcdfb73bf5afe
Saurabh Anil Wabale <saurabhwabale9797@gmail.com>
2025-10-11 12:12:18 +0530

    Add files via upload

---
+++ arraydemo1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ int Arr[4]= {10,20,30,40,50};
+
+ printf("size of Arr : %u\n",sizeof (Arr));
+
+ printf("Arr : %u\n",Arr);
+ printf("&Arr : %lu\n",&Arr);
+ 
+
+
+
+
+    return 0;
+} 
